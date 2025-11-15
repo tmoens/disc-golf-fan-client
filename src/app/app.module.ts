6910759bb9628812523db2f8d1ad2e93d550550d
Ted Moens <ted.moens@gmail.com>
2025-11-15 17:38:03 -0500

    maintenance: up to angular 18 core and cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {APP_INITIALIZER, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
@@ -13,6 +13,8 @@ import {UnauthorizedInterceptor} from './auth/unauthorized-interceptor';
 import {AppStateService} from './app-state.service';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MainMenuComponent} from './main-menu/main-menu.component';
 
 // Function to initialize AppState
 export function initializeApp(appStateService: AppStateService) {
@@ -21,30 +23,33 @@ export function initializeApp(appStateService: AppStateService) {
   };
 }
 
-@NgModule({ declarations: [
-        AppComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        MatToolbarModule,
-        MatCardModule,
-        BrowserAnimationsModule,
-        MatMenuModule,
-        MatButtonModule,
-        MatIconModule,
-        MatDividerModule], providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: UnauthorizedInterceptor,
-            multi: true
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: initializeApp,
-            deps: [AppStateService],
-            multi: true
-        },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule, ToolbarComponent, MainMenuComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UnauthorizedInterceptor,
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [AppStateService],
+      multi: true
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
+})
 export class AppModule {
 }
